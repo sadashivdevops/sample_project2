705491f49ecc7f7a20f9e7b28248510c1a67c751
sadashivdevops <sadashivudu.shirangi@calsoftinc.com>
2024-02-04 22:38:53 +0530

    added few chanes

--- git-script.sh
+++ git-script.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # Array of branch names
-branches=("Branch-1" "Branch-2" "Branch-3" "Branch-4" "Branch-5")
+branches=("Release-1" "Release-2" "Release-3" "Release-4" "Release-5")
 
 # Loop through each branch
-for branch in "${branches[@]}"
-do
-    # Checkout the branch
-    git checkout $branch
-
-    # Create a file with branch name
-    echo "This is $branch" > release-2.0.txt
-
-    # Stage the new file
-    git add release-2.0.txt
+for branch in $branches; do
+    # Create a file with the branch name
+    touch "${branch}.txt"
 
     # Commit the file in the same branch
-    git commit -m "Added release file for $branch"
+    git add "${branch}.txt"
+    git commit -m "Add file for branch ${branch}"
+
 done
+
+
+
